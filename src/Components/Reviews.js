1d5f6c93e367c6db684dc3c99b054ac16bf572ee
Alyssa Cervantes (she/her) <112597248+alyssaleanne98@users.noreply.github.com>
2022-11-29 12:35:05 -0800

    css added

--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -3,7 +3,7 @@ import React from "react";
 function Reviews() {
     return (
         <>
-        <div className="reviewContainer">
+        <div className="review">
             <h3>Reviews</h3>
             <h3>1,281</h3>
         </div>
